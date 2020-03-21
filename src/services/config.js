c4dee595a9730555c1b170c2265d8f78e5ec4240
synzen <michael994664@gmail.com>
2020-03-21 09:49:01 -0400

    Fix feed config service not returning value

--- src/services/config.js
+++ src/services/config.js
@@ -2,7 +2,7 @@ const KeyValue = require('discord.rss').KeyValue
 
 async function getFeedConfig () {
   const feedConfig = await KeyValue.get('feedConfig')
-  return feedConfig.toJSON()
+  return feedConfig.toJSON().value
 }
 
 module.exports = {
