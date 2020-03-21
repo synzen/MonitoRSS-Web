31ceb588c2ddca31e8c96055db961569d9246bb4
synzen <michael994664@gmail.com>
2020-03-20 23:26:13 -0400

    Register listeners in WebClient

--- src/structs/WebClient.js
+++ src/structs/WebClient.js
@@ -1,3 +1,5 @@
+const fs = require('fs')
+const path = require('path')
 const Discord = require('discord.js')
 const getConfig = require('../config.js').get
 const connectDatabases = require('../util/connectDatabases.js')
@@ -15,19 +17,26 @@ class WebClient {
   async login (token) {
     try {
       this.redisClient = await connectDatabases(this.config, '-')
-      const client = new Discord.Client()
-      await client.login(token)
-      this.log = createLogger(client.shard.ids[0])
+      this.client = new Discord.Client()
+      await this.client.login(token)
+      this.log = createLogger(this.client.shard.ids[0])
       this.registerListeners()
       await this.initialize()
-      client.shard.send('complete')
+      this.client.shard.send('complete')
     } catch (err) {
       throw err
     }
   }
 
   registerListeners () {
-
+    const redisClient = this.redisClient
+    const folderPath = path.join(__dirname, '..', 'events')
+    const files = fs.readdirSync(folderPath)
+    for (const name of files) {
+      const event = name.replace('.js', '')
+      const filePath = path.join(folderPath, name)
+      this.client.on(event, require(filePath)(redisClient))
+    }
   }
 
   async initialize () {
