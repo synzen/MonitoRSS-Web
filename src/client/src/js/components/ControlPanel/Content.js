518b3c80617f7233e9e2fc258879f36c03dbcbe7
synzen <michael994664@gmail.com>
2020-06-06 15:28:58 -0400

    Fix control panel not scrolling to top on route change

--- src/client/src/js/components/ControlPanel/Content.js
+++ src/client/src/js/components/ControlPanel/Content.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Switch, Route, useHistory } from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
+import { Switch, Route, useHistory, useLocation } from 'react-router-dom'
 import styled from 'styled-components'
 import Home from './Home/index'
 import Feeds from './Feeds/index'
@@ -8,7 +8,6 @@ import Message from './Message/index'
 import Filters from './Filters/index'
 import Subscribers from './Subscribers/index'
 import MiscOptions from './MiscOptions/index'
-// import Debugger from './Feed/Debugger/index'
 import { useDispatch } from 'react-redux'
 import pages from 'js/constants/pages'
 import { Scrollbars } from 'react-custom-scrollbars'
@@ -18,13 +17,20 @@ const Body = styled.div`
   height: 100%;
   width: 100%;
   background-color: #36393f;
-  /* overflow-y: auto; */
-  /* scrollbar-width: thin; */
 `
 
 function ContentBody () {
   const history = useHistory()
+  const location = useLocation()
   const dispatch = useDispatch()
+  const [scrollbarRef, setScrollbarRef] = useState()
+
+  useEffect(() => {
+    if (!scrollbarRef) {
+      return
+    }
+    scrollbarRef.scrollToTop()
+  }, [location.pathname, scrollbarRef])
 
   function redirect (page) {
     dispatch(changePage(page))
@@ -33,8 +39,7 @@ function ContentBody () {
 
   return (
     <Body>
-      {/* <Notice>Hello!</Notice> */}
-      <Scrollbars>
+      <Scrollbars ref={scrollbar => setScrollbarRef(scrollbar)}>
         <Switch>
           <Route exact path={pages.DASHBOARD} render={routerProps => <Home redirect={redirect} {...routerProps} />} />
           <Route exact path={pages.FEEDS} render={routerProps => <Feeds redirect={redirect} {...routerProps} />} />
@@ -43,7 +48,6 @@ function ContentBody () {
           <Route exact path={pages.FILTERS} render={routerProps => <Filters {...routerProps} />} />
           <Route exact path={pages.SUBSCRIBERS} render={routerProps => <Subscribers {...routerProps} />} />
           <Route exact path={pages.MISC_OPTIONS} render={routerProps => <MiscOptions {...routerProps} />} />
-          {/* <Route exact path={pages.DEBUGGER} component={routerProps => <Debugger {...routerProps} />} /> */}
           <Route render={routerProps => <Home {...routerProps} />} />
         </Switch>
       </Scrollbars>
