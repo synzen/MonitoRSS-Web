c03bb5b591cad1e2d643acf3f4959dd597869a6d
synzen <michael994664@gmail.com>
2020-06-15 20:12:37 -0400

    Add stats section to home page

---
+++ src/client/src/js/components/Home/Main/Stats.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import styled from 'styled-components'
+import { useSelector } from 'react-redux'
+import CountUp from 'react-countup'
+const Wrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin-top: -30px;
+
+  /* margin-bottom: -50px; */
+`
+
+const StatBox = styled.div`
+  padding: 30px 30px 0 30px;
+  margin: 15px 30px 0 30px;
+  max-width: 350px;
+  width: 100%;
+  text-align: center;
+  > h2 {
+    font-size: 2em;
+  }
+  > p {
+    font-size: 1.15em;
+  }
+`
+
+function Stats () {
+  const stats = useSelector(state => state.stats)
+  const {
+    articlesDelivered,
+    feedCount,
+    totalGuilds
+  } = stats
+
+  return (
+    <>
+      <h1>Our Numbers Today</h1>
+      <Wrapper>
+        <StatBox>
+          <h2>
+            <CountUp end={totalGuilds} separator=',' duration={1.5} />
+          </h2>
+          <p>Servers</p>
+        </StatBox>
+        <StatBox>
+          <h2>
+            <CountUp end={feedCount} separator=',' duration={1.5} />
+          </h2>
+          <p>News Sources</p>
+        </StatBox>
+        <StatBox>
+          <h2>
+            <CountUp end={articlesDelivered} separator=',' duration={1.5} />
+          </h2>
+          <p>Total Articles<br />Since June 2, 2020</p>
+        </StatBox>
+      </Wrapper>
+      {/* <Divider /> */}
+    </>
+  )
+}
+
+export default Stats
--- src/client/src/js/components/Home/Main/index.js
+++ src/client/src/js/components/Home/Main/index.js
@@ -3,11 +3,15 @@ import Section from './common/Section'
 import GetStarted from './GetStarted'
 import Features from './Features'
 import Header from './Header'
+import Stats from './Stats'
 
 function Main () {
   return (
     <div>
       <Header />
+      <Section>
+        <Stats />
+      </Section>
       <Section>
         <GetStarted />
       </Section>
