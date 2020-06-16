3e760046099ae851174757a4dc978e5db9246174
synzen <michael994664@gmail.com>
2020-06-15 20:18:03 -0400

    Update client build

--- src/client/build/service-worker.js
+++ src/client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.73dc9089721ab169f2ae99ad87029bd3.js"
+  "/precache-manifest.2b1d0d0f44a5caaa824c08ad7a033dd3.js"
 );
 
 self.addEventListener('message', (event) => {
