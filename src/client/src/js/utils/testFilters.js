d8a5dab8e70615a8e94b37e497ff2817c2c3b824
synzen <michael994664@gmail.com>
2021-01-23 22:13:22 -0500

    Update filter logic to match bot's

--- src/client/src/js/utils/testFilters.js
+++ src/client/src/js/utils/testFilters.js
@@ -3,10 +3,10 @@ import FilterRegex from './FilterRegex'
 import FilterResults from './FilterResults'
 
 /**
- * @param {string[]} userFilters
- * @param {string} reference
- */
-function testArrayFilters (userFilters, reference) {
+   * @param {string[]} userFilters
+   * @param {string} reference
+   */
+function testArrayNegatedFilters (userFilters, reference, findBlocks) {
   // Deal with inverted first
   const filters = userFilters.map(word => new Filter(word))
   const invertedFilters = filters.filter(filter => filter.inverted)
@@ -23,12 +23,26 @@ function testArrayFilters (userFilters, reference) {
     }
   }
 
-  if (regularFilters.length === 0) {
-    return {
-      ...returnData,
-      passed: true
-    }
+  return {
+    ...returnData,
+    passed: true
   }
+}
+
+/**
+   * @param {string[]} userFilters
+   * @param {string} reference
+   */
+function testArrayRegularFilters (userFilters, reference) {
+  // Deal with inverted first
+  const filters = userFilters.map(word => new Filter(word))
+  const invertedFilters = filters.filter(filter => filter.inverted)
+  const regularFilters = filters.filter(filter => !filter.inverted)
+  const returnData = {
+    inverted: invertedFilters.map(f => f.content),
+    regular: regularFilters.map(f => f.content)
+  }
+
   const passed = !!regularFilters.find(filter => filter.passes(reference))
   return {
     ...returnData,
@@ -69,7 +83,7 @@ function getFilterReference (article, type) {
     title: article.fullTitle
   }
   const trimmedType = type.startsWith('other:') ? type.replace('other:', '') : type
-  return referenceOverrides[trimmedType] || article[trimmedType]
+  return (referenceOverrides[trimmedType] || article[trimmedType])?.toLowerCase()
 }
 
 export default function testFilters (filters, article) {
@@ -84,37 +98,61 @@ export default function testFilters (filters, article) {
   if (!everyReferenceExists) {
     return filterResults
   }
-  let passed = false
+
   let hasOneBlock = false
+  // First check if any filters block this article
   for (const filterTypeName in filters) {
     const userFilters = filters[filterTypeName]
     const reference = getFilterReference(article, filterTypeName)
     if (!reference) {
       continue
     }
-    let invertedFilters = []
-    let regularFilters = []
-
     // Filters can either be an array of words or a string (regex)
     let results
     if (Array.isArray(userFilters)) {
-      results = testArrayFilters(userFilters, reference)
+      results = testArrayNegatedFilters(userFilters, reference)
     } else {
       results = testRegexFilter(userFilters, reference)
     }
-    passed = results.passed || passed
-    invertedFilters = invertedFilters.concat(results.inverted)
-    regularFilters = regularFilters.concat(results.regular)
+    const invertedFilters = results.inverted
+    const regularFilters = results.regular
     if (regularFilters.length > 0) {
       filterResults.add(filterTypeName, regularFilters, false)
     }
     if (invertedFilters.length > 0) {
       filterResults.add(filterTypeName, invertedFilters, true)
-      if (!results.passed) {
-        hasOneBlock = true
-      }
+    }
+    if (!results.passed) {
+      hasOneBlock = true
     }
   }
-  filterResults.passed = hasOneBlock ? false : passed
+  if (hasOneBlock) {
+    filterResults.passed = false
+    return filterResults
+  }
+  // Then do regular filters
+  let passed = false
+  let hasRegularFilters = false
+  for (const filterTypeName in filters) {
+    const userFilters = filters[filterTypeName]
+    const reference = getFilterReference(article, filterTypeName)
+    if (!reference) {
+      continue
+    }
+
+    // Filters can either be an array of words or a string (regex)
+    let results
+    if (Array.isArray(userFilters)) {
+      results = testArrayRegularFilters(userFilters, reference)
+    } else {
+      results = testRegexFilter(userFilters, reference)
+    }
+    if (results.regular.length > 0) {
+      hasRegularFilters = true
+    }
+    passed = results.passed || passed
+  }
+  // If there are no regular filters, then it should pass
+  filterResults.passed = hasRegularFilters ? passed : true
   return filterResults
 }
